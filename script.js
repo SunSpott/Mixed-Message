c6af76bf6bd67bab2e580be961b5f4aeb3815a1d
Sun Kritanat <kritanatsun@gmail.com>
2023-01-15 22:26:35 +0700

    Add string object and start function

---
+++ script.js
@@ -0,0 +1,13 @@
+let string = {
+    motto: ['Go big or go home', 'Never settle for less', 'Live, Laugh, Love', 'Work hard, play harder', 'Seize the day', 'No guts, no glory', 
+'Shoot for the moon, even if you miss, you\'ll just died', 'Dream big, work hard, stay true to yourself', 
+'Life is short, make the most of it', 'Stay hungry, stay foolish'],
+    whoShold: ['I should', 'You should', 'We should', 'They should', 'He should', 'She should'],
+    suffix: ['aren\'t I?', 'got it?', 'for sure!','!!!', 'you go, girl!', 'dude']
+}
+
+const mixedMessage = (arr) => {
+    for (prop of string) {
+        
+    }
+}
