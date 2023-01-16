4785977d847a961bae85346af19c8adac4cdb8af
Sun Kritanat <kritanatsun@gmail.com>
2023-01-16 10:05:01 +0700

    Finished my first portfolio project

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let string = {
-    motto: ['Go big or go home', 'Never settle for less', 'Live, Laugh, Love', 'Work hard, play harder', 'Seize the day', 'No guts, no glory', 
-'Shoot for the moon, even if you miss, you\'ll just died', 'Dream big, work hard, stay true to yourself', 
-'Life is short, make the most of it', 'Stay hungry, stay foolish'],
-    whoShold: ['I should', 'You should', 'We should', 'They should', 'He should', 'She should'],
-    suffix: ['aren\'t I?', 'got it?', 'for sure!','!!!', 'you go, girl!', 'dude']
+    whoShould: ['I should', 'You should', 'We should', 'They should', 'He should', 'She should'],
+    motto: ['go big or go home', 'never settle for less', 'Live, Laugh, Love', 'work hard, play harder', 'seize the day', 'no guts, no glory', 
+'shoot for the moon, even if you miss, you\'ll just died', 'dream big, work hard, stay true to yourself', 
+'life is short, make the most of it', 'stay hungry, stay foolish'],
+    suffix: ['got it?', 'for sure!', 'you go girl!', 'K dude?']
 }
 
-const mixedMessage = (arr) => {
-    for (prop of string) {
-        
+let newArr = [];
+
+for (who of string.whoShould) {
+    for (mot of string.motto) {
+        for (suf of string.suffix) {
+            newArr.push(`${who} ${mot}, ${suf}`);
+        }
     }
 }
+
+const randomize = Math.floor(Math.random() * newArr.length);
+
+console.log(newArr[randomize]);
